8f5a89ad9f934c68f6004381b306956d90aed5fb
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-09-11 08:35:31 +0300

    Create JsonPlaceHolderAPi.java

---
+++ JsonPlaceHolderAPi.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+public interface JsonPlaceHolderApi {
+    @GET("todos")
+    Call<List<Todos>> getTodos();
+}
