914c3c358a32810c991685ac686b3aa93c251c65
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-09-11 08:36:10 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,98 @@
+
+package com.example.myapplication;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    int cacheSize = 10 * 1024 * 1024;
+    
+    private TextView textViewResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewResult = findViewById(R.id.view_result);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Todos>> call = jsonPlaceHolderApi.getTodos();
+        call.enqueue(new Callback<List<Todos>>() {
+            @Override
+            public void onResponse(Call<List<Todos>> call, Response<List<Todos>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Todos> todos = response.body();
+                for (Todos todo : todos) {
+                    String content = "";
+                    content += "ID: " + todos.getId() + "\n";
+                    content += "User ID: " + todos.getUserId() + "\n";
+                    content += "Title: " + todos.getTitle() + "\n";
+                    content += "Completed: " + todos.getCompleted() + "\n\n";
+                    textViewResult.append(content);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Todos>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
+
+private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+private void loadJSON(){
+
+Cache cache = new Cache(getCacheDir(), cacheSize);
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .cache(cache)
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public okhttp3.Response intercept(Interceptor.Chain chain)
+                                throws IOException {
+                            Request request = chain.request();
+                            if (!isNetworkAvailable()) {
+                                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale \
+                                request = request
+                                        .newBuilder()
+                                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                                        .build();
+                            }
+                            return chain.proceed(request);
+                        }
+                    })
+                    .build();
+
+            Retrofit.Builder builder = new Retrofit.Builder()
+                    .baseUrl("https://jsonplaceholder.typicode.com/todos")
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+            Retrofit retrofit = builder.build();
+            Service apiService = retrofit.create(Service.class);
