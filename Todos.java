37335a90f97866ec6588c165ff460c0fa88099ea
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-09-11 08:34:34 +0300

    Create Todos.java

---
+++ Todos.java
@@ -0,0 +1,25 @@
+
+package com.example.myapplication;
+
+public class Todos {
+  
+private int userId;
+private int id;
+private String title;
+private Boolean completed;
+
+public int getUserId() {
+        return userId;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+}
+
