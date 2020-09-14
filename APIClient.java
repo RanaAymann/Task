f9084302d9342a3d087b7f0f5fd1f42b79f88c17
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-09-14 08:54:04 +0300

    Create APIClient.java

---
+++ APIClient.java
@@ -0,0 +1,12 @@
+public class ApiClient {
+    public static String BASE_URL ="https://jsonplaceholder.typicode.com/"
+    public static Retrofit getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
